eac9d665d3f95689eeaddb0156813c368322a960
replit-user <umyashinderu@gmail.com>
2025-04-26 17:00:39 -0600

    fixed a small bug

--- STACKSCRIPT.py
+++ STACKSCRIPT.py
@@ -204,9 +204,10 @@ def execute(instruction:str) -> None:
         os.system(parts[1])
     elif opcode == "OUTV":
         print(mem.top())
-    else:
+    elif not ":" in opcode and not opcode == "ENDFUNC":
         print(f"unrecognized opcode: {opcode}")
-        exit()
+    else:
+        None
 
 
 #main logic
